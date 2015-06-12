b445f193af7abe390737c66adfc884e5aaf4d023
Leah Klearman <lklrmn@gmail.com>
2015-06-12 12:49:57 -0700

    change url patterns for api to support future development

--- well-rested-tests-server/urls.py
+++ well-rested-tests-server/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'wrt.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^', include(router.urls)),
+    url(r'^api/', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
