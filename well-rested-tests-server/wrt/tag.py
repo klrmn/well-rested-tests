fc40502c4523e8c7a09cc494b1a14a546fcea341
Leah Klearman <lklrmn@gmail.com>
2015-06-16 14:17:40 -0700

    tag view

--- well-rested-tests-server/wrt/tag.py
+++ well-rested-tests-server/wrt/tag.py
@@ -3,6 +3,7 @@ from django.shortcuts import render  # views.py
 from django.contrib import admin     # admin.py
 from django.test import TestCase     # tests.py
 from rest_framework import serializers, viewsets
+from django.shortcuts import get_object_or_404, render
 
 from project import Project
 import permissions
@@ -14,6 +15,22 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+    @property
+    def passes(self):
+        return self.run_set.filter(status='pass').count()
+
+    @property
+    def fails(self):
+        return self.run_set.filter(status='fail').count()
+
+    @property
+    def inprogress(self):
+        return self.run_set.filter(status='inprogress').count()
+
+    @property
+    def aborted(self):
+        return self.run_set.filter(status='aborted').count()
 
 
 class TagAdmin(admin.ModelAdmin):
@@ -33,3 +50,21 @@ class TagViewSet(viewsets.ModelViewSet):
     serializer_class = TagSerializer
     permission_classes = (permissions.CreateOnly,)
     filter_fields = ('project',)
+
+
+def tag(request, tag_id):
+    tag = get_object_or_404(Tag, pk=tag_id)
+    runs = tag.run_set.all()
+    status = None
+    if 'status' in request.GET:
+        status = request.GET['status']
+        runs = runs.filter(status=status)
+    return render(request, 'wrt/tag.html', {
+        'tag': tag,
+        'status': status,
+        'runs': runs,
+        'passes': runs.filter(status='pass').count(),
+        'fails': runs.filter(status='fail').count(),
+        'in_progress': runs.filter(status='inprogress').count(),
+        'aborted': runs.filter(status='aborted').count(),
+    })
--- well-rested-tests-server/wrt/views.py
+++ well-rested-tests-server/wrt/views.py
@@ -4,6 +4,7 @@ from django.conf.urls import url
 from project import project, projects
 from case import case
 from run import run
+from tag import tag
 
 
 def index(request):
@@ -17,4 +18,5 @@ urlpatterns = [
     url(r'project/(?P<project_id>[0-9]+)/$', project, name='project'),
     url(r'case/(?P<case_id>[0-9]+)/$', case, name='case'),
     url(r'run/(?P<run_id>[0-9]+)/$', run, name='run'),
+    url(r'tag/(?P<tag_id>[0-9]+)/$', tag, name='tag'),
 ]
