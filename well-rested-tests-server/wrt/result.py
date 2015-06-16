4d52601e2202f40f425f4d72653af448fe600af6
Leah Klearman <lklrmn@gmail.com>
2015-06-16 14:25:09 -0700

    result and reason views/templates

--- well-rested-tests-server/wrt/result.py
+++ well-rested-tests-server/wrt/result.py
@@ -4,6 +4,7 @@ from django.contrib import admin     # admin.py
 from django.test import TestCase     # tests.py
 from rest_framework import serializers, viewsets
 from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404
 
 from run import Run
 from case import Case
@@ -41,6 +42,10 @@ class Result(models.Model):
     def project(self):
         return self.case.project
 
+    @property
+    def tags(self):
+        return self.run.tags.all()
+
 
 class ResultAdmin(admin.ModelAdmin):
     list_display = ('__str__',
@@ -62,3 +67,31 @@ class ResultViewSet(viewsets.ModelViewSet):
     serializer_class = ResultSerializer
     permission_classes = (permissions.OnlyAdminCanDelete,)
     filter_fields = ('run', 'case', 'start_time', 'owner', 'status', 'reason')
+
+
+def result(request, result_id):
+    result = get_object_or_404(Result, pk=result_id)
+    return render(request, 'wrt/result.html', {
+        'result': result,
+        'tags': result.tags,
+    })
+
+
+def reason(request, project_id, reason_name):
+    results = Result.objects.filter(run__project__id=project_id).filter(reason=reason_name)
+    passes = results.filter(status='pass').count()
+    fails = results.filter(status='fail').count()
+    xfails = results.filter(status='xfail').count()
+    xpasses = results.filter(status='xpass').count()
+    inprogress = results.filter(status='inprogress').count()
+    skips = results.filter(status='skip').count()
+    return render(request, 'wrt/reason.html', {
+        'reason': reason_name,
+        'results': results,
+        'passes': passes,
+        'fails': fails,
+        'xfails': xfails,
+        'xpasses': xpasses,
+        'inprogress': inprogress,
+        'skip': skips,
+    })
--- well-rested-tests-server/wrt/views.py
+++ well-rested-tests-server/wrt/views.py
@@ -4,6 +4,7 @@ from django.conf.urls import url
 from project import project, projects
 from case import case
 from run import run
+from result import result, reason
 from tag import tag
 
 
@@ -18,5 +19,7 @@ urlpatterns = [
     url(r'project/(?P<project_id>[0-9]+)/$', project, name='project'),
     url(r'case/(?P<case_id>[0-9]+)/$', case, name='case'),
     url(r'run/(?P<run_id>[0-9]+)/$', run, name='run'),
+    url(r'result/(?P<result_id>[0-9]+)/$', result, name='result'),
     url(r'tag/(?P<tag_id>[0-9]+)/$', tag, name='tag'),
+    url(r'reason/(?P<project_id>[0-9]+)/(?P<reason_name>[a-zA-Z1-9 _=]+)$', reason, name='reason'),
 ]
