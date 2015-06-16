cad5c0d5e1cb0c277951ef0e2d24a12e069d1b58
Leah Klearman <lklrmn@gmail.com>
2015-06-16 14:14:32 -0700

    run view

--- well-rested-tests-server/wrt/run.py
+++ well-rested-tests-server/wrt/run.py
@@ -4,6 +4,7 @@ from django.contrib import admin     # admin.py
 from django.test import TestCase     # tests.py
 from rest_framework import serializers, viewsets
 from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404
 
 from project import Project
 from tag import Tag
@@ -34,49 +35,62 @@ class Run(models.Model):
             return self.end_time - self.start_time
         return None
 
-    @property
-    def registered(self):
-        return self.tests.count()
+    def results(self):
+        from result import Result
+        return Result.objects.filter(run=self).order_by('-start_time')
 
     def __str__(self):
         return '%s %s (%s)' % (self.project, self.id, self.owner)
 
     @property
+    def reasons(self):
+        rsns = []
+        for result in self.results():
+            rsns.append(result.reason)
+        return set(rsns)
+
+    # counts
+    @property
     def tests_run(self):
-        return len(self.tests())
+        return self.results().exclude(status='exists').count()
+
+    @property
+    def passes(self):
+        return self.passed_tests().count()
 
     @property
     def failures(self):
-        return len(self.failed_tests())
+        return self.failed_tests().count()
 
     @property
     def xpasses(self):
-        return len(self.xpassed_tests())
+        return self.xpassed_tests().count()
 
     @property
     def xfails(self):
-        return len(self.xfailed_tests())
+        return self.xfailed_tests().count()
 
-    def tests(self):
-        from result import Result
-        return Result.objects.filter(run=self).filter(case__fixture=False)
+    @property
+    def skips(self):
+        return self.skipped_tests().count()
 
     def failed_tests(self):
-        return self.tests.filter(status='fail')
+        return self.results().filter(status='fail')
 
     def passed_tests(self):
-        return self.tests.filter(status='pass')
+        return self.results().filter(status='pass')
 
     def xfailed_tests(self):
-        return self.tests.filter(status='xfail')
+        return self.results().filter(status='xfail')
 
     def xpassed_tests(self):
-        return self.tests.filter(status='xpass')
+        return self.results().filter(status='xpass')
 
     def skipped_tests(self):
-        return self.tests.filter(status='skip')
+        return self.results().filter(status='skip')
 
     def description(self):
+        # used to show tags in the admin interface
         tags = [tag.name for tag in self.tags.all()]
         print(self.tags)
         return ' '.join(tags)
@@ -84,7 +98,7 @@ class Run(models.Model):
 
 class RunAdmin(admin.ModelAdmin):
     list_display = ('id', 'start_time', 'end_time', 'duration', 'status', 'description',
-                    'registered', 'tests_run', 'failures', 'xpasses', 'xfails')
+                    'results', 'tests_run', 'failures', 'xpasses', 'xfails')
     list_filter = ('project', 'owner', 'status', 'start_time')
 
 
@@ -105,3 +119,30 @@ class RunViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.OnlyAdminCanDelete,)
     # don't try to filter by tags, they can't be made into a query string
     filter_fields = ('project', 'status')
+
+
+def run(request, run_id):
+    run = get_object_or_404(Run, pk=run_id)
+    results = run.results().filter(case__fixture=False)
+    passes = results.filter(status='pass').count()
+    fails = results.filter(status='fail').count()
+    xfails = results.filter(status='xfail').count()
+    xpasses = results.filter(status='xpass').count()
+    inprogress = results.filter(status='inprogress').count()
+    skips = results.filter(status='skip').count()
+    status = None
+    if 'status' in request.GET:
+        status = request.GET['status']
+        results = results.filter(status=status)
+    return render(request, 'wrt/run.html', {
+        'status': status,
+        'run': run,
+        'tags': run.tags.all(),
+        'results': results,
+        'passes': passes,
+        'fails': fails,
+        'xfails': xfails,
+        'xpasses': xpasses,
+        'inprogress': inprogress,
+        'skip': skips,
+    })
--- well-rested-tests-server/wrt/views.py
+++ well-rested-tests-server/wrt/views.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 
 from project import project, projects
 from case import case
+from run import run
 
 
 def index(request):
@@ -15,4 +16,5 @@ urlpatterns = [
     url(r'projects/*$', projects, name='projects'),
     url(r'project/(?P<project_id>[0-9]+)/$', project, name='project'),
     url(r'case/(?P<case_id>[0-9]+)/$', case, name='case'),
+    url(r'run/(?P<run_id>[0-9]+)/$', run, name='run'),
 ]
