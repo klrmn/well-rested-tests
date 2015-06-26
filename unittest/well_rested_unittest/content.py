181e23e86367087ea8d2703d2e2ba8c1b91e0366
Leah Klearman <lklrmn@gmail.com>
2015-06-26 15:05:52 -0700

    png content

--- unittest/well_rested_unittest/content.py
+++ unittest/well_rested_unittest/content.py
@@ -9,6 +9,8 @@ URL = ContentType('text', 'x-url')
 TRACEBACK = ContentType('text', 'x-traceback',
             {"language": "python", "charset": "utf8"})
 
+PNG = ContentType('image', 'png')
+
 
 def unittest_traceback_content(err):
     return Content(TRACEBACK, lambda: traceback.format_exception(*err))
@@ -22,9 +24,13 @@ def url_content(url):
     return Content(URL, lambda: url)
 
 
+def png_content(data):
+    return Content(PNG, lambda: data)
+
+
 __all__ = [
     'Content', 'ContentType', 'content_from_file',
-    'traceback_content', 'text_content',
+    'traceback_content', 'text_content', 'png_content',
     'json_content', 'url_content', 'unittest_traceback_content',
-    'URL', 'JSON', 'UTF8_TEXT', 'TRACEBACK',
+    'URL', 'JSON', 'UTF8_TEXT', 'TRACEBACK', 'PNG',
 ]
